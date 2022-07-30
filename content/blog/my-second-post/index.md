d6b8100b9f239dfa1f0686b4a63d504ba530d606
stonesteel1023 <stonesteel1023@gmail.com>
2022-07-30 12:26:06 +0900

    first commit

--- content/blog/my-second-post/index.md
+++ content/blog/my-second-post/index.md
@@ -1,13 +1,6 @@
 ---
-title: My Second Post!
-date: "2015-05-06T23:46:37.121Z"
+title: 다시 시작
+date: "2022-07-30T12:24:00.121Z"
 ---
 
-Wow! I love blogging so much already.
-
-Did you know that "despite its name, salted duck eggs can also be made from
-chicken eggs, though the taste and texture will be somewhat different, and the
-egg yolk will be less rich."?
-([Wikipedia Link](https://en.wikipedia.org/wiki/Salted_duck_egg))
-
-Yeah, I didn't either.
+보화 바보
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `GRITsteel's new home`,
     author: {
-      name: `Kyle Mathews`,
-      summary: `who lives and works in San Francisco building useful things.`,
+      name: `stonesteel`,
+      summary: `Cyber funk agent`,
     },
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsbystarterblogsource.gatsbyjs.io/`,
+    description: `New home from gitpage`,
+    siteUrl: `https://gatsbystarterblog53830.gtsb.io/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `stonesteel1023`,
     },
   },
   plugins: [
