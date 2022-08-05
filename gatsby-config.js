f0afdcb49cd1c5e1e5ea47db4278ee632330e8c2
Michael Choi <stonesteel1023@gmail.com>
2022-08-06 01:53:53 +0900

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -44,8 +44,8 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/reading`,
-        name: `reading`,
+        path: `${__dirname}/content/scrab`,
+        name: `scrab`,
       },
     },
     {
